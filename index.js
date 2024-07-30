44d5faaf234e4d20ee7bc800612e618e4fb97160
esra <esrabaskaya84@gmail.com>
2024-07-30 09:21:50 +0200

    index.ejs ist created

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-'use strict'
+"use strict";
 /*
     EXPRESSJS 
     ! TODO PROJECT
 */
 
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-require('dotenv').config()
-const PORT=process.env?.PORT ||  8000
-const HOST=process.env?.HOST || '127.0.0.1'
+require("dotenv").config();
+const PORT = process.env?.PORT || 8000;
+const HOST = process.env?.HOST || "127.0.0.1";
 
-//catch async-error  
-require("express-async-errors")
+//catch async-error
+require("express-async-errors");
 
-// json to obj  and obj to json  
+app.set("view engine", "ejs");
+
+// json to obj  and obj to json
 //! yeri onemli yukarıda kalsın
-app.use(express.json())
+app.use(express.json());
 
 // ROUTER
-app.use(require('./src/routers/todoRouter'))
+app.use(require("./src/routers/todoRouter"));
 
 // errorHAndler
-app.use(require('./src/middlewares/errorHandler') )
+app.use(require("./src/middlewares/errorHandler"));
 
-app.listen(PORT,()=>console.log(`server runned http://${HOST}:${PORT}`))
-
+app.listen(PORT, () => console.log(`server runned http://${HOST}:${PORT}`));
